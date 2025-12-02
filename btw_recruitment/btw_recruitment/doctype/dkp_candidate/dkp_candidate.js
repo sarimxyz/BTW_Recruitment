0ce42cc2bd447f435e805d23807a0e3ab3c68556
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2025-12-02 13:00:24 +0530

    fixed the highlight issue for select fields and also refresh on save, hide prefferred location

--- btw_recruitment/btw_recruitment/doctype/dkp_candidate/dkp_candidate.js
+++ btw_recruitment/btw_recruitment/doctype/dkp_candidate/dkp_candidate.js
@@ -1,75 +1,3 @@
-// Copyright (c) 2025, Sarim and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("DKP_Candidate", {
-// 	refresh(frm) {
-
-// 	},
-// });
-// frappe.ui.form.on("DKP_Candidate", {
-//     resume_attachment: function(frm) {
-//         if (frm.doc.resume_attachment) {
-//             frappe.call({
-//                 method: "btw_recruitment.btw_recruitment.api.resume_parser.process_resume",
-//                 args: {
-//                     docname: frm.doc.name
-//                 },
-//                 freeze: true,
-//                 freeze_message: "Extracting data from resume..."
-//             }).then(r => {
-//                 frappe.msgprint("Resume processed successfully!");
-//                 frm.reload_doc();
-//             });
-//         }
-//     }
-// });
-// frappe.ui.form.on("DKP_Candidate", {
-//     resume_attachment: function(frm) {
-//         if (frm.doc.resume_attachment) {
-
-//             // ensure file is saved first
-//             frm.save().then(() => {
-//                 frappe.call({
-//                     method: "btw_recruitment.btw_recruitment.api.resume_parser.process_resume",
-//                     args: {
-//                         docname: frm.doc.name
-//                     },
-//                     freeze: true,
-//                     freeze_message: "Extracting data from resume..."
-//                 }).then(r => {
-//                     frappe.msgprint("Resume processed successfully!");
-//                     frm.reload_doc();
-//                 });
-//             });
-//         }
-//     }
-// });
-
-// frappe.ui.form.on("DKP_Candidate", {
-//     refresh(frm) {
-//         const manual_fields = [
-//             "alternate_phone",
-//             "gender",
-//             "current_ctc",
-//             "expected_ctc",
-//             "notice_period_days",
-//             "last_working_day",
-//             "resume_source"
-//         ];
-
-//         manual_fields.forEach(fieldname => {
-//             const field = frm.get_field(fieldname);
-
-//             if (field && (!frm.doc[fieldname] || frm.doc[fieldname] === "")) {
-//                 $(field.input_area).css({
-//                     "border": "2px solid #ff4d4d",
-//                     "background-color": "#ffecec",
-//                     "border-radius": "4px"
-//                 });
-//             }
-//         });
-//     }
-// });
 frappe.ui.form.on("DKP_Candidate", {
     resume_attachment(frm) {
         if (frm.doc.resume_attachment) {
@@ -93,7 +21,14 @@ frappe.ui.form.on("DKP_Candidate", {
         }
     },
 
+    // --------------------------
+    // REFRESH - HIGHLIGHT
+    // --------------------------
     refresh(frm) {
+
+        // ALWAYS remove old highlights first
+        removeHighlights(frm);
+
         // highlight only AFTER resume parsing
         if (!frm.doc.resume_parsed) return;
 
@@ -108,16 +43,46 @@ frappe.ui.form.on("DKP_Candidate", {
         ];
 
         manual_fields.forEach(fieldname => {
-            const field = frm.get_field(fieldname);
 
-            if (field && (!frm.doc[fieldname] || frm.doc[fieldname] === "")) {
-                $(field.input_area).css({
-                    "border": "2px solid #ff4d4d",
-                    "background-color": "#ffecec",
-                    "border-radius": "4px"
-                });
+            const field = frm.get_field(fieldname);
+            const value = frm.doc[fieldname];
+
+            const isEmpty =
+                value === null ||
+                value === undefined ||
+                value === "" ||
+                value === 0;
+
+            if (field && isEmpty) {
+                // field input
+                if (field.$input) {
+                    field.$input.css({
+                        "border": "2px solid #ff4d4d",
+                        "background-color": "#ffecec",
+                        "border-radius": "4px"
+                    });
+                }
             }
         });
     }
 });
+
+
+// -------------------------------------
+// CLEAR ALL HIGHLIGHTS (GLOBAL CLEANER)
+// -------------------------------------
+function removeHighlights(frm) {
+    Object.values(frm.fields_dict).forEach(field => {
+
+        if (field.$input) {
+            field.$input.css({
+                "border": "",
+                "background-color": "",
+                "border-radius": ""
+            });
+        }
+
+    });
+}
+    
 
